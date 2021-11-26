3047f096e3df7cf93f45e9534b98e6917885cba5
Darren Wong <darrenwong@Darrens-MacBook-Air.local>
2021-11-27 00:05:32 +1100

    2.1 commit

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -7,12 +7,12 @@ const Header = ({ course }) => {
   )
 }
 
-const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-  return(
-    <p>Number of exercises {sum}</p>
-  ) 
-}
+// const Total = ({ course }) => {
+//   const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+//   return(
+//     <p>Number of exercises {sum}</p>
+//   ) 
+// }
 
 const Part = (props) => {
   return (
@@ -32,32 +32,39 @@ const Content = ({ course }) => {
   )
 }
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course}/>
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
