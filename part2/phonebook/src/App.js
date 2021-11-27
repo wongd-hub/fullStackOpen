adb599939cf223e88b7338121015d80b6f0c35ce
Darren Wong <darrenwong@Darrens-MacBook-Air.local>
2021-11-27 16:15:11 +1100

    2.6 commit

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
   const [newName, setNewName] = useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setPersons([
+      ...persons,
+      {name: newName}
+    ]);
+  }
+
+  const changeHandler = (event) => setNewName(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={changeHandler}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+     {persons.map((per) => <p key={per.name}>{per.name}</p>)}
     </div>
   )
 }
