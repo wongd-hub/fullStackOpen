90accf1709d78f6130bef59021fe3b28077e6203
Darren Wong <email@darrenwong.net>
2021-12-07 19:42:29 +1100

    2.12 commit

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const Form = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form>
+      <h2>Find countries: </h2>
+      <input value={props.search} onChange={props.changeSearchHandler} />
+    </form>  
+  )
+}
+
+const CountryFeature = (props) => {
+  return (
+    <div className="country-feature">
+      <h2>{props.country.name.common}</h2>
+      <img src={props.country.flags.png} />
+      <p>Capital: {props.country.capital}</p> 
+      <p>Population: {props.country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {
+          Object
+          .values(props.country.languages)
+          .map(el => <li key={el}>{el}</li>)
+        }
+      </ul>
     </div>
+  )
+}
+
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        const tmpResults = [];
+
+        // Filter for entries where common name includes the search string
+        response.data
+          .map(el => {
+            if (el.name.common.toLowerCase().includes(search.toLowerCase())) {
+              tmpResults.push(el);
+            }
+          })
+
+        setResults(tmpResults);
+      })
+  }, [search])
+
+  const changeSearchHandler = (event) => setSearch(event.target.value);
+
+  return (
+    <>
+      <Form search={search} changeSearchHandler={changeSearchHandler} />
+      <div className="results">
+        {
+          search.length === 0 ? <p>Awaiting input</p> :
+          results.length === 0 ? <p>No results</p> :
+          results.length === 1 ? <CountryFeature country={results[0]}/> :
+          results.length > 10 ? <p>Too many matches, specify another filter</p> :
+          results.map(el => <p key={el.cioc}>{el.name.common}</p>)
+        }
+      </div>
+    </>
   );
 }
 
